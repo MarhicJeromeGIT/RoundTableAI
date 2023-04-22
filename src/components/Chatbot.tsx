8190bb9f300c96f334ed2cc8b460cfd573c2353d
Jerome <jerome@wovn.io>
2023-04-22 15:05:37 +0900

    trying to set chatbox height fixed

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -49,34 +49,36 @@ const Chatbot: React.FC = () => {
   
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex flex-col flex-1 p-4 overflow-y-auto space-y-4 bg-opacity-80 bg-[rgba(238,221,198,0.8)] rounded-md">
-        {messages.map((message) => (
-          <div
-            key={message.id}
-            className={`p-2 pl-4 pr-4 text-black break-words rounded-md ${
-              message.sender === 'user'
-                ? 'bg-[rgba(95,158,160,0.8)] self-end'
-                : 'bg-[rgba(210,180,140,0.8)] self-start'
-            }`}
+      <div className="w-full h-full">
+        <div className="flex flex-col flex-1 p-4 h-96 overflow-y-scroll space-y-4 bg-opacity-80 bg-[rgba(238,221,198,0.8)] rounded-md">
+          {messages.map((message) => (
+            <div
+              key={message.id}
+              className={`p-2 pl-4 pr-4 text-black break-words rounded-md ${
+                message.sender === 'user'
+                  ? 'bg-[rgba(95,158,160,0.8)] self-end'
+                  : 'bg-[rgba(210,180,140,0.8)] self-start'
+              }`}
+            >
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <div className="flex items-center p-4 space-x-4 bg-opacity-60 bg-[rgba(64,38,33,0.8)] border-t-2 border-[rgba(64,38,33,0.8)]">
+          <input
+            className="flex-1 p-2 text-lg text-black border-2 border-gray-400 rounded-md focus:outline-none focus:border-[rgba(95,158,160,0.8)]"
+            placeholder="Type your message..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => e.key === 'Enter' && handleSendMessage()}
+          />
+          <button
+            className="px-4 py-2 text-lg font-semibold text-white bg-[rgba(95,158,160,0.8)] rounded-md focus:outline-none focus:ring-2 focus:ring-[rgba(95,158,160,0.5)]"
+            onClick={handleSendMessage}
           >
-            {message.text}
-          </div>
-        ))}
-      </div>
-      <div className="flex items-center p-4 space-x-4 bg-opacity-60 bg-[rgba(64,38,33,0.8)] border-t-2 border-[rgba(64,38,33,0.8)]">
-        <input
-          className="flex-1 p-2 text-lg text-black border-2 border-gray-400 rounded-md focus:outline-none focus:border-[rgba(95,158,160,0.8)]"
-          placeholder="Type your message..."
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => e.key === 'Enter' && handleSendMessage()}
-        />
-        <button
-          className="px-4 py-2 text-lg font-semibold text-white bg-[rgba(95,158,160,0.8)] rounded-md focus:outline-none focus:ring-2 focus:ring-[rgba(95,158,160,0.5)]"
-          onClick={handleSendMessage}
-        >
-          Send
-        </button>
+            Send
+          </button>
+        </div>
       </div>
     </div>
   );
